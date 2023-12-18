746c1c7e6b674ec58f9cbd401b0ac87ea90762a0
Givemeliberte <97298472+Givemeliberte@users.noreply.github.com>
2023-12-18 01:12:44 -0500

    got creators working and basic form validation

---
+++ scripts/template.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    
+
+    let creatorsUrl = "../scripts/creators-data.json"
+    fetch(creatorsUrl).then(response => response.json()).then(data => buildCreatorContent(data));
+    function buildCreatorContent(data){
+        let mysource = document.getElementById("creator-template").innerHTML;
+        let mytemplate = Handlebars.compile(mysource)
+        myresult = mytemplate(data)
+        document.getElementById('container').innerHTML = myresult;
+    }
+    
+   
+
+});
